35f2e584d82eb3d659eee97a3c51f3454446a632
Alexander Talberg <alexandermtalberg@gmail.com>
2020-04-02 17:49:04 -0600

    fixed all of the styling for each type of room, now the doors willl show up no mater what door is opened

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -12,24 +12,61 @@ function Room(props) {
 
     function pickRoom() {
         switch (props.doors) {
-            case 't': return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>);
+            case 't': return (<div className={`${props.x} ${props.color} room ${props.y}`}><TopDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} /> </div>);
             case 'r': return (<div className={`${props.x} ${props.color} room ${props.y}`}><RightDoor rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></RightDoor></div>);
-            case 'b': return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>);
+            case 'b': return (<div className={`${props.x} ${props.color} room ${props.y}`}><BottomDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></BottomDoor> </div>);
             case 'l': return (<div className={`${props.x} ${props.color} room ${props.y}`}><LeftDoor rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} ></LeftDoor></div>);
-            case 't': return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>);
-            case 'trbl': return (<div className={`${props.x} ${props.color} room ${props.y}`}>>
-            <TopDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`}  location={`${props.coor}`}/>
-            <RightDoor doors={props.doors}  rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></RightDoor>
-            <BottomDoor doors={props.doors}  rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></BottomDoor>
-            <LeftDoor doors={props.doors}   rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} ></LeftDoor>
+            case 'tr': return (<div className={`${props.x} ${props.color} room ${props.y}`}>
+                <TopDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} />
+                <RightDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></RightDoor>
             </div>);
-            case 't': return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>);
-            case 't': return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>);
-            case 't': return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>);
-            case 't': return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>);
-            case 't': return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>);
-            case 't': return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>);
-            case 't': return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>);
+            case 'tb': return (<div className={`${props.x} ${props.color} room ${props.y}`}>
+                <TopDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} />
+                <BottomDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></BottomDoor>
+            </div>);
+            case 'tl': return (<div className={`${props.x} ${props.color} room ${props.y}`}>
+                <TopDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} />
+                <LeftDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} ></LeftDoor>
+
+            </div>);
+            case 'rb': return (<div className={`${props.x} ${props.color} room ${props.y}`}>
+                <RightDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></RightDoor>
+                <BottomDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></BottomDoor>
+            </div>);
+            case 'rl': return (<div className={`${props.x} ${props.color} room ${props.y}`}>
+                <RightDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></RightDoor>
+                <LeftDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} ></LeftDoor>
+            </div>);
+            case 'bl': return (<div className={`${props.x} ${props.color} room ${props.y}`}>
+                <BottomDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></BottomDoor>
+                <LeftDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} ></LeftDoor>
+            </div>);
+            case 'trb': return (<div className={`${props.x} ${props.color} room ${props.y}`}>
+                <TopDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} />
+                <RightDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></RightDoor>
+                <BottomDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></BottomDoor>
+            </div>);
+            case 'trl': return (<div className={`${props.x} ${props.color} room ${props.y}`}>
+                <TopDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} />
+                <RightDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></RightDoor>
+                <LeftDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} ></LeftDoor>
+            </div>);
+            case 'tbl': return (<div className={`${props.x} ${props.color} room ${props.y}`}>
+                <TopDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} />
+                <BottomDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></BottomDoor>
+                <LeftDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} ></LeftDoor>
+            </div>);
+            case 'rbl': return (<div className={`${props.x} ${props.color} room ${props.y}`}>
+                <RightDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></RightDoor>
+                <BottomDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></BottomDoor>
+                <LeftDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} ></LeftDoor>
+            </div>);
+            case 'trbl': return (<div className={`${props.x} ${props.color} room ${props.y}`}>>
+                <TopDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} />
+                <RightDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></RightDoor>
+                <BottomDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`}></BottomDoor>
+                <LeftDoor doors={props.doors} rooms={props.rooms} changeState={props.changeState} open={props.open} x={`${props.x} `} y={`${props.y}`} location={`${props.coor}`} ></LeftDoor>
+            </div>);
             default: return (<div className={`${props.x} ${props.color} room ${props.y}`}></div>)
 
 
